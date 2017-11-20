2bce3bd3272bec007048aa4ac83596b2cd8025e5
JosephKevin <jkm2155@columbia.edu>
2017-11-20 00:03:44 -0500

    added new methods to star class

--- nearest_stars/star.py
+++ nearest_stars/star.py
@@ -2,6 +2,7 @@ class star(object):
 	"""
 	Class to represent star object
 	"""
+	
 	def __init__(self, x, y, z, star_name):
 		self.x = x
 		self.y = y
@@ -13,3 +14,14 @@ class star(object):
 
 	def print(self):
 		print('Star Name: {0}, x: {1}, y: {2}, z: {3}'.format(self.star_name, self.x, self.y, self.z))
+	
+	def __hash__(self):
+		return hash((self.x, self.y, self.z, self.star_name))
+
+	def __eq__(self, other):
+		return (self.x, self.y, self.z, self.star_name) == (other.x, other.y, other.z, other.star_name)
+
+	def __ne__(self, other):
+		# Not strictly necessary, but to avoid having both x==y and x!=y
+		# True at the same time
+		return not(self == other)
