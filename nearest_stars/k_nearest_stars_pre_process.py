932ebd7f36deff31e78d7e59164180f112587afe
JosephKevin <jkm2155@columbia.edu>
2017-11-20 01:55:56 -0500

    added map for star name to id

--- nearest_stars/k_nearest_stars_pre_process.py
+++ nearest_stars/k_nearest_stars_pre_process.py
@@ -14,22 +14,29 @@ def pre_process(n_trees, idx_file, dim = 3):
 	Args:
 		n_trees: Number of trees to be built
 		idx_file: the file mapping id to 
+		id_obj: mapping from id to star object
+		obj_id: mapping from star name to id
 		Reference: https://github.com/spotify/annoy
 
 	Returns:
 		No Return 
 	"""
-	id_vct = {}
+	id_obj = {}
+	obj_id = {}
 	utils.skip_line(1)
 	tree = AnnoyIndex(dim, metric='euclidean')
 	idx = 0
 	for line in fileinput.input():
 		star_name, x, y, z = utils.parse_line(line)
 		tree.add_item(idx, [x, y, z])
-		id_vct[idx] = star(x,y,z, star_name)
+		id_obj[idx] = star(x,y,z, star_name)
+		obj_id[star_name] = idx
 		idx += 1
-	pickle_out = open("./dict.pickle","wb")
-	pickle.dump(id_vct, pickle_out)
+	pickle_out = open("./id_obj.pickle","wb")
+	pickle.dump(id_obj, pickle_out)
+	pickle_out.close()
+	pickle_out = open("./obj_id.pickle","wb")
+	pickle.dump(obj_id, pickle_out)
 	pickle_out.close()
 	tree.build(n_trees)
 	tree.save(idx_file)
